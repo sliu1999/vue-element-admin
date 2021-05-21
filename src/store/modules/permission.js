ee0014f4bffbeb00626b8dce3337ced5e3fc7263
sliu <1622436253@qq.com>
2021-05-21 10:47:28 +0800

    实现动态路由

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,4 +1,8 @@
-import { asyncRoutes, constantRoutes } from '@/router'
+import { constantRoutes } from '@/router'
+import Layout from '@/layout'
+import { getToken} from '@/utils/auth'
+import { getRoutes } from '@/api/user'
+
 
 /**
  * Use meta.role to determine if the current user has permission
@@ -18,23 +22,32 @@ function hasPermission(roles, route) {
  * @param routes asyncRoutes
  * @param roles
  */
-export function filterAsyncRoutes(routes, roles) {
+ // 递归权限过滤和数据转换
+ export function filterAsyncRoutes(routes, roles) {
   const res = []
-
   routes.forEach(route => {
-    const tmp = { ...route }
+    const tmp = {...route}
     if (hasPermission(roles, tmp)) {
-      if (tmp.children) {
-        tmp.children = filterAsyncRoutes(tmp.children, roles)
+      const component = tmp.component
+      if (route.component) {
+        if (component === 'Layout') {
+          tmp.component = Layout
+        } else {
+          // 接口组件字符串转换成组件对象
+          tmp.component = (resolve) => require([`@/views/${component}`], resolve)
+        }
+        if (tmp.children) {
+          tmp.children = filterAsyncRoutes(tmp.children, roles)
+        }
       }
       res.push(tmp)
     }
   })
-
   return res
 }
 
 const state = {
+  token: getToken(),
   routes: [],
   addRoutes: []
 }
@@ -49,14 +62,13 @@ const mutations = {
 const actions = {
   generateRoutes({ commit }, roles) {
     return new Promise(resolve => {
-      let accessedRoutes
-      if (roles.includes('admin')) {
-        accessedRoutes = asyncRoutes || []
-      } else {
-        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
-      }
-      commit('SET_ROUTES', accessedRoutes)
-      resolve(accessedRoutes)
+      // 请求后台数据替换src/router/index.js的asyncRoutes异步路由
+      getRoutes(state.token).then(response => {
+        // filterAsyncRoutes方法作权限过滤和数据转换，roles为登录用户角色ID集合，如：[1,2]
+        const accessedRoutes = filterAsyncRoutes(response.data, roles)
+        commit('SET_ROUTES', accessedRoutes)
+        resolve(accessedRoutes)
+      })
     })
   }
 }
